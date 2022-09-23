327f111c60cb1d5f665e3a7b048794aa31ea9a1c
itsssjudyyy <jzheng20@students.desu.edu>
2022-09-23 16:28:31 -0400

    hi

--- src/main/java/BasicInput.java
+++ src/main/java/BasicInput.java
@@ -5,10 +5,24 @@ public class BasicInput {
         Scanner scnr = new Scanner(System.in);
         int userInt = 0;
         double userDouble = 0.0;
+        char userChar;
+        String userString;
         // TODO Define char and string variables similarly
+
 
         System.out.println("Enter integer:");
         userInt = scnr.nextInt();
+
+        System.out.println("Enter double:");
+        userDouble = scnr.nextDouble();
+
+        System.out.println("Enter charecter:");
+        userChar = scnr.next().charAt(0);
+
+        System.out.println("Enter String:");
+        userString = scnr.next();
+
+        System.out.println(userString + " " + userChar + " " + userDouble + " " + userInt);
 
         // TODO (1): Finish reading other items into variables, then output the four values on a single line separated by a space
 
@@ -16,6 +30,6 @@ public class BasicInput {
 
         // TODO (3): Cast the double to an integer, and output that integer
 
-        return;
+
     }
 }
